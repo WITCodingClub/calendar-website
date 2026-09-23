205ad11731493634336559f49d15801b8d3baf33
Daniel Miretsky <miretskyd@wit.edu>
2026-09-23 09:38:00 -0400

    Update button text for extension installation on home page
    Signed-off-by: Daniel Miretsky <miretskyd@wit.edu>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -51,7 +51,7 @@
         More Info
     </Button>
     <Button variant={extension.status === "installed" ? "outlined" : "filled"} onclick={() => window.open(webStoreUrlFor(navigator.userAgent), '_blank', 'noopener,noreferrer')}><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d={extension.status === "installed" ? "M9 16.17L4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41z" : "m16 8.4l-8.9 8.9q-.275.275-.7.275t-.7-.275t-.275-.7t.275-.7L14.6 7H7q-.425 0-.712-.288T6 6t.288-.712T7 5h10q.425 0 .713.288T18 6v10q0 .425-.288.713T17 17t-.712-.288T16 16z"}/></svg>
-        {extension.status === "installed" ? "Installed" : "Web Store"}
+        {extension.status === "installed" ? "Installed" : "Install extension"}
     </Button>
         {#if authStatus?.authenticated}
         <Button variant="filled" onclick={() => window.location.href = authStatus?.admin ? "/admin" : "/dashboard"}>
